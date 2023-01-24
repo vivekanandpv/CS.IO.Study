e555c37b89257afac3c9bcf5c0efae4526631fe4
Vivekanand P V <vivekanandpv@outlook.com>
2023-01-24 21:12:12 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,35 @@
-﻿using System;
+﻿namespace CS.IO.Study;
 
-Console.WriteLine("Hello, world!");
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        ReadBinaryFile();
+    }
+
+    static void ReadTextFile()
+    {
+        FileStream fs = File.Open(Path.Combine("/","Users","vivekanandpv","hello.txt"), FileMode.Open);
+
+        using (TextReader reader = new StreamReader(fs))
+        {
+            string content = reader.ReadToEnd();
+
+            Console.WriteLine(content);
+        }
+    }
+
+    static void ReadBinaryFile()
+    {
+        FileStream fs = File.Open(Path.Combine("/","Users","vivekanandpv","horse.jpeg"), FileMode.Open);
+
+        using (BinaryReader reader = new BinaryReader(fs))
+        {
+            while (reader.BaseStream.Position != reader.BaseStream.Length)
+            {
+                int b = reader.ReadByte();
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
