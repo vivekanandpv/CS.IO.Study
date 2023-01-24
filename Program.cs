af23ef9aa82bafd6415c2c846ab3c0b72dd67bf6
Vivekanand P V <vivekanandpv@outlook.com>
2023-01-24 21:36:56 +0530

    done

--- Program.cs
+++ Program.cs
@@ -7,23 +7,16 @@ internal class Program
         ReadBinaryFile();
     }
 
-    static void ReadTextFile()
-    {
-        FileStream fs = File.Open(Path.Combine("/","Users","vivekanandpv","hello.txt"), FileMode.Open);
-
-        using (TextReader reader = new StreamReader(fs))
-        {
-            string content = reader.ReadToEnd();
-
-            Console.WriteLine(content);
-        }
-    }
-
     static void ReadBinaryFile()
     {
         FileStream fs = File.Open(Path.Combine("/","Users","vivekanandpv","horse.jpeg"), FileMode.Open);
 
-        using (BinaryReader reader = new BinaryReader(fs))
+        //  decorator pattern
+        //  BinaryReader is an adapter
+        //  BufferedStream is a decorator
+        //  FileStream is a backing store
+        //  10000 is the buffer size in bytes (10KB)
+        using (BinaryReader reader = new BinaryReader(new BufferedStream(fs, 10000)))
         {
             while (reader.BaseStream.Position != reader.BaseStream.Length)
             {
